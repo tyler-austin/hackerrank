90ba2379b9a70d151acef5933553667d7242a23c
Tyler <tyler.e.austin@gmail.com>
2019-04-05 16:26:12 -0400

    completed freq query

--- src/dictionaries-and-hashmaps/frequency-query.js
+++ src/dictionaries-and-hashmaps/frequency-query.js
@@ -2,23 +2,22 @@ const freqQuery = queries => {
   const result = [];
   const lookup = {};
   const freqs = {};
-  // https://www.hackerrank.com/challenges/frequency-queries/forum/comments/476734
   for (let i = 0; i < queries.length; i++) {
     const [op, data] = queries[i];
     const freq = lookup.hasOwnProperty(data) ? lookup[data] : 0;
+    if (typeof freqs[freq] === 'undefined') freqs[freq] = new Set();
     if (op === 1) {
       lookup[data] = freq + 1;
-      
-      freq.delete(freq);
-      freq.add(freq + 1);
+      freqs[freq].delete(data);
+      if (typeof freqs[freq + 1] === 'undefined') freqs[freq + 1] = new Set();
+      freqs[freq + 1].add(data);
     } else if (op === 2) {
-      if (lookup[data] === 1) delete lookup[data];
-      else if (lookup[data] > 1) lookup[data] -= 1;
-
-    } else {
-      // TODO: see if freqs Set contains data
-      result.push(Object.values(lookup).indexOf(data) > -1 ? 1 : 0);
-    }
+      lookup[data] = Math.max(0, freq - 1);
+      freqs[freq].delete(data);
+      if (typeof freqs[freq - 1] === 'undefined') freqs[freq - 1] = new Set();
+      freqs[freq - 1].add(data);
+    } else if (typeof freqs[data] === 'undefined') result.push(0);
+    else result.push(freqs[data].size > 0 ? 1 : 0);
   }
   return result;
 };
