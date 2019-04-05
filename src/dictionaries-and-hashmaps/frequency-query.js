66f7db7f3638e13c0478d49571112a36f84458fa
Tyler <tyler.e.austin@gmail.com>
2019-04-05 06:51:26 -0400

    working on freq query

--- src/dictionaries-and-hashmaps/frequency-query.js
+++ src/dictionaries-and-hashmaps/frequency-query.js
@@ -1,19 +1,20 @@
 const freqQuery = queries => {
   const result = [];
   const lookup = {};
-  const freqs = new Set();
-  // TODO: maintain set of freqs
+  const freqs = {};
   // https://www.hackerrank.com/challenges/frequency-queries/forum/comments/476734
   for (let i = 0; i < queries.length; i++) {
     const [op, data] = queries[i];
+    const freq = lookup.hasOwnProperty(data) ? lookup[data] : 0;
     if (op === 1) {
-      if (lookup.hasOwnProperty(data)) lookup[data] += 1;
-      else {
-        lookup[data] = 1;
-      }
+      lookup[data] = freq + 1;
+      
+      freq.delete(freq);
+      freq.add(freq + 1);
     } else if (op === 2) {
-      if (lookup.hasOwnProperty(data) && lookup[data] === 1) delete lookup[data];
-      else if (lookup.hasOwnProperty(data) && lookup[data] > 1) lookup[data] -= 1;
+      if (lookup[data] === 1) delete lookup[data];
+      else if (lookup[data] > 1) lookup[data] -= 1;
+
     } else {
       // TODO: see if freqs Set contains data
       result.push(Object.values(lookup).indexOf(data) > -1 ? 1 : 0);
